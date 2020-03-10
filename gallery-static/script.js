be39c0ad31c8650daa2698110c26f7e333b5bea8
asifZaman0362 <zero362001@gmail.com>
2020-03-10 14:27:21 +0530

    finish gallery

--- gallery-static/script.js
+++ gallery-static/script.js
@@ -1,32 +1,45 @@
-var im_path = "../res/images/";
-var im_extension = ".jpg";
-var image_elements = [];
-var column_size = 4;
-var columns = [];
-var curr_column = 0;
-
 window.addEventListener("load", onDocumentLoad);
 
 function onDocumentLoad() {
-    for (i = 0; i < column_size; i++) {
+
+    const PATH = "../res/images/";
+    const EXTENSION = ".jpg";
+    const COL_SIZE = 4;
+
+    var columns = [];
+    var curr_column = 0;
+
+    var no_columns = Math.floor(files.length / COL_SIZE);
+    
+    for (i = 0; i < COL_SIZE; i++) {
         columns[i] = document.createElement("DIV");
         columns[i].classList.add("column");
         document.getElementsByClassName("row")[0].appendChild(columns[i]);
-        // document.getElementsByClassName("row")[0].style.height = 
     }
+    
     for (i = 0; i < files.length; i++) {
-        if (i % Math.floor(files.length / column_size) == 0) {
+        
+        if (i % no_columns == 0) {
             curr_column++;
         }
-        var a = document.createElement("A");
-        a.href = im_path + files[i] + im_extension;
+
+        var hyperlink = document.createElement("A");
+        hyperlink.href = PATH + files[i] + EXTENSION;
+
+        var column_child = document.createElement("DIV");
+        column_child.style.width = "100%";
+        var height = (100 / Math.floor(files.length / COL_SIZE));
+        column_child.style.height = height + "%";
+
+        var img_container = document.createElement("DIV");
+        img_container.classList.add("img_container");
+        
         var img = document.createElement("IMG");
-        img.src = im_path + files[i] + im_extension;
-        img.style.width = "100%";
-        var height = (100 / Math.floor(files.length / column_size));
-        console.log(files.length/column_size);
-        img.style.height = height + "%";
-        a.appendChild(img);
-        columns[curr_column-1].appendChild(a);
+        img.src = hyperlink.href;
+
+        hyperlink.appendChild(img);
+        img_container.appendChild(hyperlink);
+        column_child.appendChild(img_container);
+        columns[curr_column-1].appendChild(column_child);
     }
 }
